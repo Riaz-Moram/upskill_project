c10742e79011a93b60bfa9b7ec8cd5fad7f6f064
Your Name <your_email@example.com>
2025-09-29 16:28:17 +0430

    i make the main structure for my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Styles from "./Style";
 import { Navbar } from "./Components/index";
-
 export default function App() {
   return (
     <>
-      <Navbar />
+      <div className="bg-primary w-full overflow-hidden text-white">
+        <div className={`${Styles.paddingX} ${Styles.flexCenter}`}>
+          <div className={`${Styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
